35e01f25ecfd3d67e30eedecfeced592679fb9a0
gaofeng222 <596642721@qq.com>
2025-05-17 05:20:08 +0800

    add:新增css3GPU加速文章

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,112 +1,112 @@
-import { defineConfig } from "vitepress";
+import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  lang: "zh-Hans",
-  title: "高峰的个人博客",
-  description: "技术博客爱好者,专注于web开发技术分享。",
-  base: "/gaofeng-blog/",
+  lang: 'zh-Hans',
+  title: '高峰的个人博客',
+  description: '技术博客爱好者,专注于web开发技术分享。',
+  base: '/gaofeng-blog/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: "首页", link: "/" },
-      { text: "示例", link: "/about-vue3" },
+      { text: '首页', link: '/' },
+      { text: '示例', link: '/about-vue3' }
     ],
     outline: {
-      label: "本页内容",
+      label: '本页内容'
     },
     sidebar: [
       {
-        text: "html5",
+        text: 'html5',
         items: [
-          { text: "h5中的rem", link: "/src/articles/h5/rem.md" },
+          { text: 'h5中的rem', link: '/src/articles/h5/rem.md' }
           // { text: "Runtime API Examples", link: "/api-examples" },
-        ],
-      },
-      // {
-      //   text: "css3",
-      //   items: [
-      //     // { text: "Markdown Examples", link: "/markdown-examples" },
-      //     // { text: "Runtime API Examples", link: "/api-examples" },
-      //   ],
-      // },
-      {
-        text: "数据结构",
-        items: [
-          // { text: "Markdown Examples", link: "/markdown-examples" },
-          // { text: "Runtime API Examples", link: "/api-examples" },
-        ],
+        ]
       },
       {
-        text: "基础算法",
+        text: 'css3',
+        items: [
+          { text: 'css3中的GPU加速', link: '/markdown-examples' }
+          // { text: "Runtime API Examples", link: "/api-examples" },
+        ]
+      },
+      {
+        text: '数据结构',
         items: [
           // { text: "Markdown Examples", link: "/markdown-examples" },
           // { text: "Runtime API Examples", link: "/api-examples" },
-        ],
+        ]
       },
       {
-        text: "前端框架之Vue3",
+        text: '基础算法',
+        items: [
+          // { text: "Markdown Examples", link: "/markdown-examples" },
+          // { text: "Runtime API Examples", link: "/api-examples" },
+        ]
+      },
+      {
+        text: '前端框架之Vue3',
 
         items: [
-          { text: "杂谈vue3", link: "/src/articles/about-vue3" },
-          { text: "Runtime API Examples", link: "/api-examples" },
-        ],
+          { text: '杂谈vue3', link: '/src/articles/about-vue3' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
       },
       {
-        text: "前端框架之React",
+        text: '前端框架之React',
 
         items: [
           {
-            text: "craco.config配置文件的记录",
-            link: "/src/articles/react/craco-config",
-          },
+            text: 'craco.config配置文件的记录',
+            link: '/src/articles/react/craco-config'
+          }
           // { text: "Runtime API Examples", link: "/api-examples" },
-        ],
+        ]
       },
       {
-        text: "前端框架之Angular",
+        text: '前端框架之Angular',
 
         items: [
           // { text: "Markdown Examples", link: "/markdown-examples" },
           // { text: "Runtime API Examples", link: "/api-examples" },
-        ],
+        ]
       },
       {
-        text: "后端Java",
+        text: '后端Java',
 
         items: [
           // { text: "Markdown Examples", link: "/markdown-examples" },
           // { text: "Runtime API Examples", link: "/api-examples" },
-        ],
+        ]
       },
       {
-        text: "后端框架springboot",
+        text: '后端框架springboot',
         items: [
           // { text: "Markdown Examples", link: "/markdown-examples" },
           // { text: "Runtime API Examples", link: "/api-examples" },
-        ],
+        ]
       },
       {
-        text: "数据库mysql",
+        text: '数据库mysql',
         items: [
           // { text: "Markdown Examples", link: "/markdown-examples" },
           // { text: "Runtime API Examples", link: "/api-examples" },
-        ],
-      },
+        ]
+      }
     ],
     docFooter: {
-      prev: "上一页",
-      next: "下一页",
+      prev: '上一页',
+      next: '下一页'
     },
     notFound: {
-      title: "页面未找到",
+      title: '页面未找到',
       quote:
-        "但如果你不改变方向，并且继续寻找，你可能最终会到达你所前往的地方。",
-      linkLabel: "前往首页",
-      linkText: "带我回首页",
+        '但如果你不改变方向，并且继续寻找，你可能最终会到达你所前往的地方。',
+      linkLabel: '前往首页',
+      linkText: '带我回首页'
     },
     socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" },
-    ],
-  },
-});
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
